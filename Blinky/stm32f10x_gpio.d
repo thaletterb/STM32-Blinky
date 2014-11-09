4fb7dbcf5cfd717ab0beb25cc5f2bb6ab9b38791
Brian <thaletterb@gmail.com>
2014-11-09 15:00:37 -0800

    add compiled .elf

---
+++ Blinky/stm32f10x_gpio.d
@@ -0,0 +1,8 @@
+stm32f10x_gpio.o:  \
+ /home/brian/Projects/stm/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_gpio.c \
+  /home/brian/Projects/stm/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_gpio.h \
+  /home/brian/Projects/stm/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/stm32f10x.h \
+  /home/brian/Projects/stm/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/CoreSupport/core_cm3.h \
+  /home/brian/Projects/stm/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.h \
+  ../stm32f10x_conf.h \
+  /home/brian/Projects/stm/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_rcc.h
